8a2abadf3c1fba3a6b76541a6f0e12dc1240be68
Miel-cyb <109626691+Miel-cyb@users.noreply.github.com>
2023-06-20 13:10:57 +0000

    Add files via upload

--- Online Voting Site/app.js
+++ Online Voting Site/app.js
@@ -1,3 +1,4 @@
+//Swapping from login and signUp
 const signUpBtn = document.querySelector("#signUp");
 const loginBtn = document.querySelector("#login");
 const login = document.querySelector("#loginForm");
@@ -19,3 +20,63 @@ function swap() {
 	signUpForm.style.left = "50px";
 	btn.style.left = "110px";
 }
+
+
+//Validating the form
+    const emailInput = document.querySelector(".email");
+	const  username = document.querySelector(".username");
+	const createPassword = document.querySelector(".create-password");;
+	const confirmPassword = document.querySelector(".confirm-password");
+	
+
+// // username
+function checkUsername(){
+   if(username.value === ""){
+	alert("Username can't be empty");
+   }
+   else if(username.value <=2){
+	alert("Username must be at least 3 characters")
+   }
+   return "Success";
+}
+	
+// // email
+function checkEmail(){
+	const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ if (emailInput.value === ""){
+	alert("email field can't be empty");
+ }
+ else if (!emailInput.value.match(pattern)){
+	alert("email must contain some characters,@ symbol and a domain(e.g .com)");
+ } 
+}
+
+// password
+ function checkPassword(){
+	const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+	if(createPassword.value ===""){
+		alert("password can't be empty")
+	}
+	else if(!createPassword.value.match(pattern)){
+		alert("Password be at least 8 characters long: must contain a number,lowercase and uppercase")
+	}
+ }
+ function confirmCheck(){
+	if(!confirmPassword.value.match(createPassword)){
+		alert("Password mismatch")
+	}
+ }
+
+signUpForm.addEventListener("submit", function (e) {
+	e.preventDefault(); //preventing form from submitting
+	checkUsername();
+	checkEmail();
+	checkPassword();
+	confirmCheck();
+});
+
+
+      
+		
+
+	
