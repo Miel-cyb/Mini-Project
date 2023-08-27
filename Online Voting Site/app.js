0d8264724904a6cfec114f430a2ef17a6d73a7c8
Miel-cyb <109626691+Miel-cyb@users.noreply.github.com>
2023-08-27 17:30:28 +0000

    Add files via upload

--- Online Voting Site/app.js
+++ Online Voting Site/app.js
@@ -16,137 +16,6 @@ close_btn.addEventListener("click", function () {
 
   
 
-    
-    // document.addEventListener("DOMContentLoaded", function () {
-	// 		const storedSelection = localStorage.getItem("candidateSelection");
-	// 		const candidateSelection = storedSelection
-	// 			? JSON.parse(storedSelection)
-	// 			: {};
-
-	// 		const voteButtons = document.querySelectorAll(".vote");
-	// 		const removeButtons = document.querySelectorAll(".remove-vote");
-	// 		const votesResults = {}; // To store the vote counts
-
-	// 		voteButtons.forEach((button) => {
-	// 			button.addEventListener("click", voteHandler);
-	// 		});
-
-	// 		removeButtons.forEach((button) => {
-	// 			button.addEventListener("click", removeVoteHandler);
-	// 		});
-
-	// 		// Initialize vote counts from candidateSelection
-	// 		for (const position in candidateSelection) {
-	// 			const candidateName = candidateSelection[position];
-	// 			votesResults[candidateName] = (votesResults[candidateName] || 0) + 1;
-	// 			updateVoteCount(candidateName);
-	// 		}
-
-	// 		// Function to update the vote count on the UI
-	// 		function updateVoteCount(candidateName) {
-	// 			const voteSpan = document.querySelector(
-	// 				`span[data-id="${candidateName}"]`
-	// 			);
-	// 			const currentVotes = votesResults[candidateName] || 0;
-	// 			voteSpan.textContent = currentVotes;
-	// 		}
-
-	// 		function voteHandler(event) {
-	// 			event.preventDefault();
-	// 			const button = event.target;
-	// 			const candidateName = button.getAttribute("data-id").trim();
-	// 			const candidatePosition = button.getAttribute("data-category").trim();
-
-	// 			const previousSelection = candidateSelection[candidatePosition];
-
-	// 			if (!previousSelection) {
-	// 				candidateSelection[candidatePosition] = candidateName;
-	// 				localStorage.setItem(
-	// 					"candidateSelection",
-	// 					JSON.stringify(candidateSelection)
-	// 				);
-
-	// 				votesResults[candidateName] = (votesResults[candidateName] || 0) + 1;
-	// 				updateVoteCount(candidateName);
-
-	// 				// Show remove vote button
-	// 				const removeButton = document.querySelector(
-	// 					`.remove-vote[data-id="${candidateName}"]`
-	// 				);
-	// 				removeButton.style.display = "inline";
-	// 			} else if (previousSelection === candidateName) {
-	// 				alert(
-	// 					`You have already voted for ${candidateName} for the position of ${candidatePosition}.`
-	// 				);
-	// 			} else {
-	// 				alert(
-	// 					`A candidate has already been selected for the position of ${candidatePosition}.`
-	// 				);
-	// 			}
-	// 		}
-
-	// 		function removeVoteHandler(event) {
-	// 			event.preventDefault();
-	// 			const button = event.target;
-	// 			const candidateName = button.getAttribute("data-id").trim();
-	// 			const candidatePosition = button.getAttribute("data-category").trim();
-
-	// 			if (candidateSelection[candidatePosition] === candidateName) {
-	// 				votesResults[candidateName] = (votesResults[candidateName] || 0) - 1;
-	// 				updateVoteCount(candidateName);
-
-	// 				// Remove selection
-	// 				delete candidateSelection[candidatePosition];
-	// 				localStorage.setItem(
-	// 					"candidateSelection",
-	// 					JSON.stringify(candidateSelection)
-	// 				);
-
-	// 				// Hide remove vote button
-	// 				button.style.display = "none";
-
-	// 				// Enable vote button
-	// 				const voteButton = document.querySelector(
-	// 					`.vote[data-id="${candidateName}"][data-category="${candidatePosition}"]`
-	// 				);
-	// 				voteButton.disabled = false;
-	// 			}
-	// 		}
-
-	// 		const submitButton = document.getElementById("submit");
-	// 		submitButton.addEventListener("click", submitHandler);
-
-	// 		function submitHandler(event) {
-	// 			event.preventDefault();
-
-	// 			// Check if the user has voted by checking the candidateSelection
-	// 			const selectedPositions = Object.keys(candidateSelection);
-
-	// 			if (selectedPositions.length === 0) {
-	// 				alert("Please vote first before submitting.");
-	// 			} else {
-	// 				alert("Vote submitted!");
-	// 				// window.location.href =""
-
-	// 				// Reset candidateSelection in local storage for new users to vote again
-	// 				localStorage.removeItem("candidateSelection");
-
-	// 				// Reset the candidateSelection in memory for the current user
-	// 				for (const position in candidateSelection) {
-	// 					delete candidateSelection[position];
-	// 				}
-
-	// 				// Update vote counts and UI display
-	// 				for (const candidateName of Object.keys(votesResults)) {
-	// 					updateVoteCount(candidateName);
-	// 				}
-	// 			}
-	// 		}
-
-	// 	});
-
-
-
 
 
 
@@ -155,6 +24,8 @@ close_btn.addEventListener("click", function () {
 			const candidateSelection = storedSelection
 				? JSON.parse(storedSelection)
 				: {};
+             
+			const userDetails = JSON.parse(localStorage.getItem("userDetails"));
 
 			const voteButtons = document.querySelectorAll(".vote");
 			const removeButtons = document.querySelectorAll(".remove-vote");
@@ -263,6 +134,10 @@ function voteHandler(event) {
 				} else {
 					alert("Vote submitted!");
 
+					// Update user's voting status in userDetails
+					userDetails.hasVoted = true;
+					localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
 					// Reset candidateSelection for new users to vote again
 					localStorage.removeItem("candidateSelection");
 
@@ -273,8 +148,9 @@ function voteHandler(event) {
 					for (const candidateName of Object.keys(votesResults)) {
 						updateVoteCount(candidateName);
 					}
-					window.location.href ="index.html"
+					window.location.href = "index.html";
 				}
 				
 			}
-		});
+		
+		})
